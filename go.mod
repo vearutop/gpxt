26cdaced8cec24e90455619c4e2a2579e88a0876
Viacheslav Poturaev <vearutop@gmail.com>
2022-05-29 18:09:22 +0200

    initial

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/alecthomas/kingpin v2.2.6+incompatible
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20210208195552-ff826a37aa15 // indirect
-	github.com/bool64/dev v0.1.29
-	github.com/tkrajina/gpxgo v1.0.1
+	github.com/bool64/dev v0.2.12
+	github.com/tkrajina/gpxgo v1.2.1
+	golang.org/x/net v0.0.0-20220526153639-5463443f8c37 // indirect
 )
---
+++ gpxt.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/alecthomas/kingpin"
+	"io/ioutil"
+	"time"
+
+	"github.com/tkrajina/gpxgo/gpx"
+)
+
+func main() {
+	timeCmd()
+	infoCmd()
+	mergeCmd()
+
+	kingpin.Parse()
+}
+
+func infoCmd() {
+	var file string
+
+	info := kingpin.Command("info", "Show info about GPX file")
+	info.Arg("file", "File to show info for.").Required().StringVar(&file)
+	info.Action(func(_ *kingpin.ParseContext) error {
+		gpxFile, err := gpx.ParseFile(file)
+		if err != nil {
+			return fmt.Errorf("error opening gpx file: %w", err)
+		}
+
+		fmt.Println(GetGpxElementInfo("", gpxFile))
+
+		return nil
+	})
+}
+
+func timeCmd() {
+	var (
+		file     string
+		newStart string
+		newEnd   string
+	)
+
+	cmd := kingpin.Command("time", "Move track in time")
+	cmd.Arg("file", "GPX File to process.").Required().StringVar(&file)
+	cmd.Flag("new-start", "New time of track start, e.g. 2022-05-28T10:36:34Z.").StringVar(&newStart)
+	cmd.Flag("new-end", "New time of track end, e.g. 2022-05-28T10:36:34Z.").StringVar(&newEnd)
+
+	cmd.Action(func(_ *kingpin.ParseContext) error {
+		gpxFile, err := gpx.ParseFile(file)
+		if err != nil {
+			return fmt.Errorf("error opening gpx file: %w", err)
+		}
+
+		var delta time.Duration
+
+		if newStart != "" {
+			start, err := time.Parse(time.RFC3339, newStart)
+			if err != nil {
+				return fmt.Errorf("failed to parse new start time: %w")
+			}
+
+			delta = start.Sub(gpxFile.TimeBounds().StartTime)
+		}
+
+		if newEnd != "" {
+			end, err := time.Parse(time.RFC3339, newEnd)
+			if err != nil {
+				return fmt.Errorf("failed to parse new end time: %w")
+			}
+
+			delta = end.Sub(gpxFile.TimeBounds().EndTime)
+
+		}
+
+		if delta == 0 {
+			fmt.Println("no delta to apply")
+
+			return nil
+		}
+
+		fmt.Println("delta to apply:", delta.String())
+
+		gpxFile.ExecuteOnAllPoints(func(point *gpx.GPXPoint) {
+			point.Timestamp = point.Timestamp.Add(delta)
+		})
+
+		fmt.Println(GetGpxElementInfo("", gpxFile))
+
+		xx, err := gpxFile.ToXml(gpx.ToXmlParams{})
+		if err != nil {
+			return fmt.Errorf("error rendering GPX: %w", err)
+		}
+
+		if err = ioutil.WriteFile("./out.gpx", xx, 0o660); err != nil {
+			return fmt.Errorf("failed to save GPX file: %w", err)
+		}
+
+		return nil
+	})
+
+}
+
+func mergeCmd() {
+	var files []string
+
+	merge := kingpin.Command("merge", "Merge multiple GPX tracks in one")
+	merge.Arg("files", "Files to merge.").StringsVar(&files)
+	merge.Action(func(_ *kingpin.ParseContext) error {
+		if len(files) < 2 {
+			return errors.New("at least two files expected for merge")
+		}
+
+		gpxFile, err := gpx.ParseFile(files[0])
+		if err != nil {
+			return fmt.Errorf("error opening gpx file: %w", err)
+		}
+
+		for _, f := range files[1:] {
+			mf, err := gpx.ParseFile(f)
+			if err != nil {
+				return fmt.Errorf("error opening gpx file: %w", err)
+			}
+
+			for _, t := range mf.Tracks {
+				for _, s := range t.Segments {
+					gpxFile.AppendSegment(&s)
+				}
+			}
+		}
+
+		xx, err := gpxFile.ToXml(gpx.ToXmlParams{})
+		if err != nil {
+			return fmt.Errorf("error rendering GPX: %w", err)
+		}
+
+		if err = ioutil.WriteFile("./out.gpx", xx, 0o660); err != nil {
+			return fmt.Errorf("failed to save GPX file: %w", err)
+		}
+
+		return nil
+	})
+}
+
+// GetGpxElementInfo pretty prints some basic information about this GPX elements
+func GetGpxElementInfo(prefix string, gpxDoc gpx.GPXElementInfo) string {
+	result := ""
+	result += fmt.Sprint(prefix, " Points: ", gpxDoc.GetTrackPointsNo(), "\n")
+	result += fmt.Sprint(prefix, " Length 2D: ", gpxDoc.Length2D()/1000.0, "\n")
+	result += fmt.Sprint(prefix, " Length 3D: ", gpxDoc.Length3D()/1000.0, "\n")
+
+	bounds := gpxDoc.Bounds()
+	result += fmt.Sprintf("%s Bounds: %f, %f, %f, %f\n", prefix, bounds.MinLatitude, bounds.MaxLatitude, bounds.MinLongitude, bounds.MaxLongitude)
+
+	md := gpxDoc.MovingData()
+	result += fmt.Sprint(prefix, " Moving time: ", (time.Duration(md.MovingTime) * time.Second).String(), "\n")
+	result += fmt.Sprint(prefix, " Stopped time: ", (time.Duration(md.StoppedTime) * time.Second).String(), "\n")
+
+	result += fmt.Sprintf("%s Max speed: %fm/s = %fkm/h\n", prefix, md.MaxSpeed, md.MaxSpeed*60*60/1000.0)
+
+	updo := gpxDoc.UphillDownhill()
+	result += fmt.Sprint(prefix, " Total uphill: ", updo.Uphill, "\n")
+	result += fmt.Sprint(prefix, " Total downhill: ", updo.Downhill, "\n")
+
+	timeBounds := gpxDoc.TimeBounds()
+	result += fmt.Sprint(prefix, " Started: ", timeBounds.StartTime.Format(time.RFC3339), "\n")
+	result += fmt.Sprint(prefix, " Ended: ", timeBounds.EndTime.Format(time.RFC3339), "\n")
+	return result
+}
